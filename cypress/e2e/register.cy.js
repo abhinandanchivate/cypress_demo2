67cf9a1ef3ad3c4f0cb3e9248c13ff499d805979
abhinandanchivate <abhinadanchivate2000@gmail.com>
2025-06-21 06:03:53 +0530

    updated one

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,47 +1,53 @@
 describe("User Registration flow", () => {
   const timestamp = Date.now();
   const user = {
-    name: "John Doe",
+    name: `john${timestamp}`,
+    lastName: `doe${timestamp}`,
     email: `john${timestamp}@example.com`,
     password: "password123",
   };
   beforeEach(() => {
-    cy.visit("/register");
+    cy.visit("/register.htm");
     cy.log("navigated to /register");
+    Cypress.on("window:before:load", (win) => {
+      Object.defineProperty(win.navigator, "userAgent", {
+        value:
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113 Safari/537.36",
+      });
+    });
   });
   it("should register a new user", () => {
-    cy.get('[data-testid="register-name"]')
-      .type(user.name)
-      .should("have.value", user.name);
+    cy.get('form:has(input[name="customer.firstName"])').within(() => {
+      cy.get('input[name="customer.firstName"]').type(user.name);
+      cy.get('input[name="customer.lastName"]').type(user.lastName);
+      // address
+      cy.get('input[name="customer.address.street"]').type("123 main st");
 
-    cy.get('[data-testid="register-email"]')
-      .type(user.email)
-      .should("have.value", user.email);
+      // city
+      cy.get('input[name="customer.address.city"]').type("New York");
+      // state
+      cy.get('input[name="customer.address.state"]').type("NY");
+      // zip
+      cy.get('input[name="customer.address.zipCode"]').type("10001");
+      // phone
+      cy.get('input[name="customer.phoneNumber"]').type("1234567890");
+      // ssn
+      cy.get('input[name="customer.ssn"]').type("123456789");
+      // username
+      cy.get('input[name="customer.username"]').type(user.email);
 
-    cy.get('[data-testid="register-password"]')
-      .type(user.password)
-      .should("have.value", user.password);
-
-    cy.get('[data-testid="register-password-confirmation"]')
-      .type(user.password)
-      .should("have.value", user.password);
-
-    cy.get('[data-testid="register-submit"]').click();
-    cy.url().should("include", "/dashboard");
-    cy.contains("Dashboard").should("be.visible");
-
-    // we need to handle the form
-    // form based on react / angular / vue
-    // data-testid : is a custom attribute often added to HTML elements (usually in react angular or vue) to make UI testing more stable and reliable.
-    // WHY ?
-    // DOM :
-    // dom tree may have some elements :
-    // 1. dynamic Class Names(from css modules)
-    // changing content i18N
-    // no unique identifiers. form
-    // <input type='text>
-    // <input type='email'>
-    // <input type='password'>
-    // cy.get('input')==> where to type the data
+      cy.get('input[name="customer.password"]').type(user.password);
+      cy.get('input[name="repeatedPassword"]').type(user.password);
+      cy.get('input[type="submit"]').click();
+      // cy.get('form:has(input[name="customer.firstName"])').submit();
+      // cy.get("form").submit();
+      // cy.get('form[name="customerForm"] input[type="submit"]')
+      //   .then(($els) => console.log("found", $els.length))
+      //   .click();
+      // assert
+      cy.pause();
+      cy.get('button:contains("Verify")').click();
+      cy.contains("Your account was created successfully").should("be.visible");
+    });
   });
 });
